99519c484beaf5fbc8cac4ee2713cc5c656e3bd4
Vidit Agrawal <viditagrawal56@gmail.com>
2024-04-24 01:18:34 +0530

    backend

--- backend/middlewares/accessHandler.js
+++ backend/middlewares/accessHandler.js
@@ -2,7 +2,6 @@ const accessHandler = (req, res, next) => {
   console.log("Test");
   res.set("Access-Control-Allow-Origin", "https://wt-medical-1.onrender.com");
   res.set("Access-Control-Allow-Credentials", "true");
-
   next();
 };
 
