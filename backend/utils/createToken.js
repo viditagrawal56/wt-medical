4edfa7b546ee247129d1b5d11deff52b3687f8f5
Vidit Agrawal <viditagrawal56@gmail.com>
2024-04-24 00:11:01 +0530

    backend

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,3 +1,23 @@
+// import jwt from "jsonwebtoken";
+
+// const generateToken = (res, userId) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//     expiresIn: "30d",
+//   });
+
+//   // Set JWT as an HTTP-Only Cookie
+//   res.cookie("jwt", token, {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV !== "development",
+//     sameSite: "none",
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//   });
+
+//   return token;
+// };
+
+// export default generateToken;
+
 import jwt from "jsonwebtoken";
 
 const generateToken = (res, userId) => {
@@ -8,8 +28,8 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only Cookie
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production", // Only secure in production
+    sameSite: process.env.NODE_ENV === "development" ? "none" : "strict", // Adjust sameSite based on environment
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
